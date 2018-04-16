caaec3ae35d18d1728dfa46f06a6b146fa7ddc9d
m0cchi <boom.boom.planet@gmail.com>
2018-04-17 02:07:57 +0900

    add mock server

---
+++ cmd/postal_worker/postal_worker.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/m0cchi/postal_worker/lib"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// HandleAPI is sugoi
+func HandleAPI(w http.ResponseWriter, r *http.Request) {
+	b, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		log.Println(err)
+		return
+	}
+
+	// Unmarshal
+	var postalMatter model.PostalMatter
+	err = json.Unmarshal(b, &postalMatter)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		log.Println(err)
+		return
+	}
+	json.NewEncoder(w).Encode(postalMatter)
+}
+
+func main() {
+	log.Println("start postal workerd")
+
+	http.HandleFunc("/api/register", HandleAPI)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
