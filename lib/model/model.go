ec4bf5297e66a748a93523b9f32593ec82f8eae0
m0cchi <boom.boom.planet@gmail.com>
2018-04-28 01:35:59 +0900

    PostalMatter has multiple destinations

--- lib/model/model.go
+++ lib/model/model.go
@@ -2,12 +2,16 @@ package model
 
 import "time"
 
+type To struct {
+	ModuleName string `json:"module_name"`
+	Address    string `json:"address"`
+}
+
 // PostalMatter is ...
 type PostalMatter struct {
-	ModuleName string    `json:"module_name"`
-	Body       string    `json:"body"`
-	Format     string    `json:"format"`
-	To         string    `json:"to"`
-	From       string    `json:"from"`
-	Date       time.Time `json:"date"`
+	Body   string    `json:"body"`
+	Format string    `json:"format"`
+	To     []To      `json:"to"`
+	From   string    `json:"from"`
+	Date   time.Time `json:"date"`
 }
